6edb33a5e003456e4554fb2fa55d9a1a45e575cb
Samarth Allamudi <s4m@macbook-pro.mgm.suurstoffi.juwo.ch>
2022-04-22 14:48:05 +0200

    Initial commit Added External Repositories: JUnit version 4.13.2 JUnit Jupiter Engine version 5.8.2 JUnit Vintage Engine version 5.8.2 Mockito Core version 4.5.0

---
+++ src/main/java/ch/hslu/sweng/team5/App.java
@@ -0,0 +1,13 @@
+package ch.hslu.sweng.team5;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
